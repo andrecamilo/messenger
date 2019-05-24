e72c9159843a0823725f582346d336ac11015d0e
Andre Camilo <andrehcamilo@gmail.com>
2019-05-23 21:19:20 -0300

    add storage database

--- app/src/main/java/com/letsbuildthatapp/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/letsbuildthatapp/kotlinmessenger/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.letsbuildthatapp.kotlinmessenger
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -14,20 +15,32 @@ class LoginActivity: AppCompatActivity() {
     setContentView(R.layout.activity_login)
 
     login_button_login.setOnClickListener {
-      val email = email_edittext_login.text.toString()
-      val password = password_edittext_login.text.toString()
-
-      Log.d("Login", "Attempt login with email/pw: $email/***")
-
-      FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-//          .addOnCompleteListener()
-//          .add
-
+      performLogin()
     }
 
     back_to_register_textview.setOnClickListener{
       finish()
     }
+  }
+
+  private fun performLogin() {
+    val email = email_edittext_login.text.toString()
+    val password = password_edittext_login.text.toString()
+
+    if (email.isEmpty() || password.isEmpty()) {
+      Toast.makeText(this, "Please fill out email/pw.", Toast.LENGTH_SHORT).show()
+      return
+    }
+
+    FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener {
+          if (!it.isSuccessful) return@addOnCompleteListener
+
+          Log.d("Login", "Successfully logged in: ${it.result.user.uid}")
+        }
+        .addOnFailureListener {
+          Toast.makeText(this, "Failed to log in: ${it.message}", Toast.LENGTH_SHORT).show()
+        }
   }
 
 }
